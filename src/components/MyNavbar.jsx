f286cc784033c5e1c3c21ad176521d8609e4c8b7
yksl <yukselkurtuldu9@gmail.com>
2024-04-15 13:26:47 +0300

    image edit

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -6,7 +6,7 @@ const MyNavbar = ({blogCards}) => {
         <a className="text-decoration-none text-black" href="#" role="button">Posts <sup className="bg-danger rounded-circle p-1 text-warning">{blogCards.length}</sup></a>
           <a href="#" role="button"><i className="fa-solid fa-bars px-2 text-black"></i></a>
           <a href="#" role="button"><i className="fa-solid fa-bell px-2 text-black"></i></a>
-          <a href="#" role="button"><img className="image" src={"https://ca.slack-edge.com/T06BXPTNG1H-U06BK5LFNDD-35895782569c-512"} /></a>        
+          <a href="#" role="button"><img className="image rounded-5" src={"https://ca.slack-edge.com/T06BXPTNG1H-U06BK5LFNDD-35895782569c-512"} /></a>        
         </div>
     </nav>
   );
